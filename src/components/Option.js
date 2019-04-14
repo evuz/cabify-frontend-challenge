62850e1aa277fd2c4fd22c6b532a4db7e5ae7c07
evuz <jesus_jgb_45@hotmail.com>
2019-04-14 08:42:00 +0200

    Select: style options

---
+++ src/components/Option.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Option({ children }) {
+  return <div className="select-option">{children}</div>;
+}
+
+export default Option;
--- src/components/Select.js
+++ src/components/Select.js
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import filterClassnames from '../utils/filterClassnames';
+import Modal from './Modal';
 
-function Select({ name, label, value, disabled }) {
+function Options({ children }) {
+  return (
+    <div className="select-options">
+      <div className="options-content">{children}</div>
+    </div>
+  );
+}
+
+function Select({ children, name, label, value, disabled }) {
+  const cmpRef = useRef(null);
   const [focus, setFocus] = useState(false);
+  disabled = disabled || !children;
+
   const containerClass = {
     focus,
     disabled,
@@ -13,20 +25,36 @@ function Select({ name, label, value, disabled }) {
     active: value,
   };
   return (
-    <div className={filterClassnames(containerClass)}>
+    <div ref={cmpRef} className={filterClassnames(containerClass)}>
       <div
         onClick={() => disabled || setFocus(true)}
-        role="listbox"
+        role="button"
         className="select"
       >
         <div className="select-value">{value}</div>
+        <input type="hidden" value={value} />
         <label htmlFor={name}>{label}</label>
       </div>
+      {focus ? (
+        <Modal className="formField-select" onClose={() => setFocus(false)}>
+          <div
+            style={{
+              position: 'absolute',
+              top: `${cmpRef.current.clientHeight +
+                cmpRef.current.offsetTop}px`,
+              left: `${cmpRef.current.offsetLeft}px`,
+            }}
+          >
+            <Options>{children}</Options>
+          </div>
+        </Modal>
+      ) : null}
     </div>
   );
 }
 
 Select.propTypes = {
+  children: PropTypes.node,
   name: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   disabled: PropTypes.bool,
@@ -34,6 +62,7 @@ Select.propTypes = {
 
 Select.defaultProps = {
   disabled: false,
+  children: null,
 };
 
 export default Select;
