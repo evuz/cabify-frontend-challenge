afa977c0f6e7fd78aaea62b2f0653ca2875ac35a
evuz <jesus_jgb_45@hotmail.com>
2019-04-17 17:00:29 +0200

    Add some validators

---
+++ src/utils/validators/index.js
@@ -0,0 +1,6 @@
+import requiredValidator from './required';
+import emailValidator from './email';
+
+export const required = requiredValidator;
+export const email = emailValidator;
+export default { required, email };
