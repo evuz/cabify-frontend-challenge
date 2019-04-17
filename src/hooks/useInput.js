cf4e42cfffb8112258d4fae80c7355eff80c2143
evuz <jesus_jgb_45@hotmail.com>
2019-04-17 17:03:09 +0200

    Hook useInput allow validators

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,13 +1,31 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 
-function useInput(initialValue = '') {
+function useInput(initialValue = '', validators = []) {
   const [value, setValue] = useState(initialValue);
+  const [errors, setErrors] = useState({ valid: true, error: {} });
+
+  useEffect(() => {
+    const validatorError = validators.reduce((acc, validator) => {
+      const error = validator(value);
+      if (error) {
+        acc[error.key] = error.message;
+      }
+      return acc;
+    }, {});
+    const errorChange =
+      JSON.stringify(validatorError) !== JSON.stringify(errors.error);
+
+    if (errorChange) {
+      const areError = !!Object.keys(validatorError).length;
+      setErrors({ valid: !areError, error: validatorError });
+    }
+  }, [value]);
 
   const onChange = useCallback(function(event) {
     setValue(event.target.value);
   });
 
-  return [value, onChange];
+  return [value, onChange, errors];
 }
 
 export default useInput;
